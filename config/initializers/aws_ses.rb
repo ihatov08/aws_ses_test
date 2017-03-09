98915be5053f2febcf5b76a7387952176c8d4f60
tomoya <ihatov08@gmail.com>
2017-03-09 13:30:01 +0900

    [modify]aws_sdkのregionを変更

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,4 +1,4 @@
-Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, { region: 'us-east-1' })
+Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, { region: 'us-west-2' })
 
 access_key = ENV['AWS_ACCESS_KEY_ID']
 secret_key = ENV['AWS_SECRET_ACCESS_KEY']
